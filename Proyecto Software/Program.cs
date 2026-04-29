6a6347ea39b9594086e9adb7d121ed6f9c3b50f2
Juan Lechares <juanlechares.ing@gmail.com>
2026-04-28 23:39:32 -0300

    Haciendo home (front end), armando esqueleto de html y css

--- Proyecto Software/Program.cs
+++ Proyecto Software/Program.cs
@@ -7,8 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
+builder.Services.AddControllers().AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
     });
@@ -22,63 +21,6 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
-
-
-//custom
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    try
-    {
-        var context = services.GetRequiredService<AppDbContext>();
-
-        // Crea la base de datos y las tablas basadas en tu OnModelCreating.
-        context.Database.EnsureCreated();
-
-        // Precarga inicial (1 Evento, 2 Sectores, 50 butacas por sector).
-        if (!context.EVENTS.Any())
-        {
-            var evento = new EVENT
-            {
-                Name = "Piratas del caribe",
-                EventDate = DateTime.Now.AddDays(30),
-                Venue = "Sala 1",
-                Status = "Active" // El campo requerido para evitar errores.
-            };
-
-            for (int s = 1; s <= 2; s++)
-            {
-                var sector = new SECTOR
-                {
-                    Name = $"Sector {s}",
-                    Price = 5000 * s,
-                    Capacity = 50
-                };
-
-                for (int b = 1; b <= 50; b++)
-                {
-                    sector.Seats.Add(new SEAT
-                    {
-                        Id = Guid.NewGuid(), // UUID como pide tu esquema.
-                        RowIdentifier = "A",
-                        SeatNumber = b,
-                        Status = "Available",
-                        Version = 1
-                    });
-                }
-                evento.Sectors.Add(sector);
-            }
-
-            context.EVENTS.Add(evento);
-            context.SaveChanges();
-            Console.WriteLine("--> Base de datos generada y precargada con éxito.");
-        }
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Error al iniciar la base de datos: {ex.Message}");
-    }
-}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
